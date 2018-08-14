4c69d58173b8a880c4a0039abfc7697a9dfa0fcf
marcelledi <42271433+marcelledi@users.noreply.github.com>
2018-08-14 09:04:18 -0300

    Update MockGpsChecker.java

--- src/android/com/sandata/MockGpsChecker.java
+++ src/android/com/sandata/MockGpsChecker.java
@@ -31,7 +31,7 @@ public class MockGpsChecker extends CordovaPlugin{
     @Override
     public boolean execute(String action, JSONArray data, final CallbackContext callbackContext) throws JSONException {
         mContext = this;
-	contextGeral = this.cordova.getActivity().getContext(); 
+	contextGeral = this.cordova.getActivity().getApplicationContext(); 
         if (action.equals("check")) {
             objGPS = new JSONObject();
             if (android.os.Build.VERSION.SDK_INT < 18) {
